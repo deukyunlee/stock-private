9118c8d23c0fc7b921faf5ed74d995714135bb20
deukyunlee <dukedev1004@gmail.com>
2022-04-21 00:02:23 +0900

    test

--- web.js
+++ web.js
@@ -12,16 +12,15 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "111111",
-  database: "capstone",
+  host: "teststock.cafe24app.com",
+  user: "dufqkd1004",
+  password: "capstone!!",
+  database: "dufqkd1004",
   port: "3306",
   multipleStatements: true,
   // dateStrings: "date",
   //socketPath: socket_path,
 });
-
 // db.connect(function (error) {
 //   if (error) {
 //     console.log(error);
