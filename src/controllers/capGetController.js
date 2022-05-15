87ecd28c87a35a6afc617915c018ee137eda99d8
deukyunlee <dukedev1004@gmail.com>
2022-05-15 20:26:05 +0900

    test

--- src/controllers/capGetController.js
+++ src/controllers/capGetController.js
@@ -5,6 +5,7 @@ module.exports.stock_cap_en_recent_fully_get = (req, res, next) => {
   const sql = `select d.symbol as symbol,c.name_kr as name,d.cap from daily as d inner join company_info as c on d.symbol = c.symbol where d.date = "2022-05-05" and d.cap is not null order by cap desc;`;
   db.query(sql, symbol, function (err, rows, fields) {
     res.json(rows);
+    //
   });
 };
 
