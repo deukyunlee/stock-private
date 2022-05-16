86a5e328070b80f37f096e616f882b1be6e41564
deukyunlee <dukedev1004@gmail.com>
2022-05-16 22:46:46 +0900

    t

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const swaggerUI = require("swagger-ui-express");
 const swaggerJSDoc = require("swagger-jsdoc");
-
+const app = require("express")();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
