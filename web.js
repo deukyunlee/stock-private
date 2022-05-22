66d4e5b5d89ea993adcd0c7d7808c2ada5c3da54
deukyunlee <dukedev1004@gmail.com>
2022-05-22 18:20:53 +0900

    commit

--- web.js
+++ web.js
@@ -32,27 +32,27 @@ const swaggerSpec = swaggerJSDoc({
   ],
 });
 
-// const db = mysql.createConnection({
-//   host: "teststock.cafe24app.com",
-//   user: "dufqkd1004",
-//   password: "capstone!!",
-//   database: "dufqkd1004",
-//   port: "3306",
-//   multipleStatements: true,
-//   // dateStrings: "date",
-//   //socketPath: socket_path,
-// });
-
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "111111",
-  database: "capstone",
+  host: "teststock.cafe24app.com",
+  user: "dufqkd1004",
+  password: "capstone!!",
+  database: "dufqkd1004",
   port: "3306",
   multipleStatements: true,
   // dateStrings: "date",
   //socketPath: socket_path,
 });
+
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "111111",
+//   database: "capstone",
+//   port: "3306",
+//   multipleStatements: true,
+//   // dateStrings: "date",
+//   //socketPath: socket_path,
+// });
 
 // db.connect(function (error) {
 //   if (error) {
