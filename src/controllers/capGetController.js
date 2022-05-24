30ac00e90c747280d7f6ce941e3fabc9cfee9755
deukyunlee <dukedev1004@gmail.com>
2022-05-24 16:26:18 +0900

    t

--- src/controllers/capGetController.js
+++ src/controllers/capGetController.js
@@ -2,7 +2,7 @@ const db = require("../../web.js");
 
 module.exports.stock_cap_en_recent_fully_get = (req, res, next) => {
   const symbol = req.params.symbol;
-  const sql = `select c.img as img, d.symbol as symbol,c.name_kr as name,d.cap from daily as d inner join company_info as c on d.symbol = c.symbol where d.date = "2022-05-05" and d.cap is not null and d.change_percent is not null order by cap desc;`;
+  const sql = `select c.img as img, d.symbol as symbol,c.name_kr as name,d.cap from daily as d inner join company_info as c on d.symbol = c.symbol where d.date = "2022-05-05" and d.cap is not null and d.change_percent is not null and c.img is not null order by cap desc;`;
   db.query(sql, symbol, function (err, rows, fields) {
     res.json(rows);
     //
@@ -11,7 +11,7 @@ module.exports.stock_cap_en_recent_fully_get = (req, res, next) => {
 
 module.exports.stock_cap_en_recent_top100_get = (req, res, next) => {
   const symbol = req.params.symbol;
-  const sql = `select c.img as img, d.symbol as symbol,c.name_kr as name,d.cap from daily as d inner join company_info as c on d.symbol = c.symbol where d.date = "2022-05-05" and d.cap is not null and d.change_percent is not null order by cap desc limit 100;`;
+  const sql = `select c.img as img, d.symbol as symbol,c.name_kr as name,d.cap from daily as d inner join company_info as c on d.symbol = c.symbol where d.date = "2022-05-05" and d.cap is not null and d.change_percent is not null and c.img is not null order by cap desc limit 100;`;
   db.query(sql, symbol, function (err, rows, fields) {
     res.json(rows);
   });
@@ -19,14 +19,14 @@ module.exports.stock_cap_en_recent_top100_get = (req, res, next) => {
 
 module.exports.stock_cap_kr_recent_fully_get = (req, res, next) => {
   const symbol = req.params.symbol;
-  const sql = `select c.img as img, d.symbol as symbol,c.name_kr as name,d.cap*1269 as cap from daily as d inner join company_info as c on d.symbol = c.symbol where d.date = "2022-05-05" and d.cap is not null and d.change_percent is not null order by cap desc;`;
+  const sql = `select c.img as img, d.symbol as symbol,c.name_kr as name,d.cap*1269 as cap from daily as d inner join company_info as c on d.symbol = c.symbol where d.date = "2022-05-05" and d.cap is not null and d.change_percent is not null and c.img is not null order by cap desc;`;
   db.query(sql, symbol, function (err, rows, fields) {
     res.json(rows);
   });
 };
 module.exports.stock_cap_kr_recent_top100_get = (req, res, next) => {
   const symbol = req.params.symbol;
-  const sql = `select c.img as img, d.symbol as symbol,c.name_kr as name,d.cap*1269 as cap from daily as d inner join company_info as c on d.symbol = c.symbol where d.date = "2022-05-05" and d.cap is not null and d.change_percent is not null order by cap desc limit 100;`;
+  const sql = `select c.img as img, d.symbol as symbol,c.name_kr as name,d.cap*1269 as cap from daily as d inner join company_info as c on d.symbol = c.symbol where d.date = "2022-05-05" and d.cap is not null and d.change_percent is not null and c.img is not null order by cap desc limit 100;`;
   db.query(sql, symbol, function (err, rows, fields) {
     res.json(rows);
   });
