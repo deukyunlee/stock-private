4a59c10516b5c87155a00fc221161ac6909a06dd
deukyunlee <dukedev1004@gmail.com>
2022-05-25 14:09:56 +0900

    t

--- src/controllers/fluctationGetController.js
+++ src/controllers/fluctationGetController.js
@@ -2,7 +2,7 @@ const db = require("../../web.js");
 
 module.exports.stock_fluctation_desc_recent_fully_get = (req, res, next) => {
   const symbol = req.params.symbol;
-  const sql = `select c.img as img, d.close,if(d.change_percent>0,concat("+",ROUND(d.change_percent,1)),ROUND(d.change_percent,1)) as change_percent, if(d.change_value>0,concat("+",d.change_value*1269),d.change_value*1269) as change_value,d.symbol as symbol,c.name_kr as name from daily as d inner join company_info as c on d.symbol=c.symbol where d.date = "2022-05-05" and img is not null and change_percent is not null order by d.change_percent desc;`;
+  const sql = `select c.img as img, d.close,if(d.change_percent>0,concat("+",ROUND(d.change_percent,1)),ROUND(d.change_percent,1)) as change_percent, if(d.change_value>0,concat("+",d.change_value*1269),change_value*1269),d.change_value*1269) as change_value,d.symbol as symbol,c.name_kr as name from daily as d inner join company_info as c on d.symbol=c.symbol where d.date = "2022-05-05" and img is not null and change_percent is not null order by d.change_percent desc;`;
   db.query(sql, symbol, function (err, rows, fields) {
     res.json(rows);
   });
@@ -10,7 +10,7 @@ module.exports.stock_fluctation_desc_recent_fully_get = (req, res, next) => {
 
 module.exports.stock_fluctation_asc_recent_fully_get = (req, res, next) => {
   const symbol = req.params.symbol;
-  const sql = `select c.img as img, d.close,if(d.change_percent>0,concat("+",ROUND(d.change_percent,1)),ROUND(d.change_percent,1)) as change_percent, if(d.change_value>0,concat("+",d.change_value*1269),d.change_value*1269) as change_value,d.symbol as symbol,c.name_kr as name from daily as d inner join company_info as c on d.symbol=c.symbol where d.date = "2022-05-05" and img is not null and change_percent is not null order by d.change_percent asc;`;
+  const sql = `select c.img as img, d.close,if(d.change_percent>0,concat("+",ROUND(d.change_percent,1)),ROUND(d.change_percent,1)) as change_percent, if(d.change_value>0,concat("+",d.change_value*1269),change_value*1269),d.change_value*1269) as change_value,d.symbol as symbol,c.name_kr as name from daily as d inner join company_info as c on d.symbol=c.symbol where d.date = "2022-05-05" and img is not null and change_percent is not null order by d.change_percent asc;`;
   db.query(sql, symbol, function (err, rows, fields) {
     res.json(rows);
     //
@@ -19,7 +19,7 @@ module.exports.stock_fluctation_asc_recent_fully_get = (req, res, next) => {
 
 module.exports.stock_fluctation_desc_recent_top100_get = (req, res, next) => {
   const symbol = req.params.symbol;
-  const sql = `select c.img as img, d.close,if(d.change_percent>0,concat("+",ROUND(d.change_percent,1)),ROUND(d.change_percent,1)) as change_percent, if(d.change_value>0,concat("+",d.change_value*1269),d.change_value*1269) as change_value,d.symbol as symbol,c.name_kr as name from daily as d inner join company_info as c on d.symbol=c.symbol where d.date = "2022-05-05" and img is not null and change_percent is not null order by d.change_percent desc limit 100;`;
+  const sql = `select c.img as img, d.close,if(d.change_percent>0,concat("+",ROUND(d.change_percent,1)),ROUND(d.change_percent,1)) as change_percent, if(d.change_value>0,concat("+",d.change_value*1269),change_value*1269),d.change_value*1269) as change_value,d.symbol as symbol,c.name_kr as name from daily as d inner join company_info as c on d.symbol=c.symbol where d.date = "2022-05-05" and img is not null and change_percent is not null order by d.change_percent desc limit 100;`;
   db.query(sql, symbol, function (err, rows, fields) {
     res.json(rows);
   });
@@ -27,7 +27,7 @@ module.exports.stock_fluctation_desc_recent_top100_get = (req, res, next) => {
 
 module.exports.stock_fluctation_asc_recent_top100_get = (req, res, next) => {
   const symbol = req.params.symbol;
-  const sql = `select c.img as img, d.close,,if(d.change_percent>0,concat("+",ROUND(d.change_percent,1)),ROUND(d.change_percent,1)) as change_percent, if(d.change_value>0,concat("+",d.change_value*1269),d.change_value*1269) as change_value,d.symbol as symbol,c.name_kr as name from daily as d inner join company_info as c on d.symbol=c.symbol where d.date = "2022-05-05" and img is not null and change_percent is not null order by d.change_percent asc limit 100;`;
+  const sql = `select c.img as img, d.close,if(d.change_percent>0,concat("+",ROUND(d.change_percent,1)),ROUND(d.change_percent,1)) as change_percent, if(d.change_value>0,concat("+",d.change_value*1269),change_value*1269) as change_value,d.symbol as symbol,c.name_kr as name from daily as d inner join company_info as c on d.symbol=c.symbol where d.date = "2022-05-05" and img is not null and change_percent is not null order by d.change_percent asc limit 100;`;
   db.query(sql, symbol, function (err, rows, fields) {
     res.json(rows);
   });
