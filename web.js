cc761accd04f73f87b9580f6db8392c914b9806f
deukyunlee <dukedev1004@gmail.com>
2022-05-28 20:46:25 +0900

    t

--- web.js
+++ web.js
@@ -33,23 +33,23 @@ const swaggerSpec = swaggerJSDoc({
   ],
 });
 
-// const db = mysql.createConnection({
-//   host: "teststock.cafe24app.com",
-//   user: "dufqkd1004",
-//   password: "capstone!!",
-//   database: "dufqkd1004",
-//   port: "3306",
-//   multipleStatements: true,
-//   typeCast: function (field, next) {
-//     if (field.type == "VAR_STRING") {
-//       return field.string();
-//     }
-//     return next();
-//   },
-//   multipleStatements: true,
-//   // dateStrings: "date",
-//   //socketPath: socket_path,
-// });
+const db = mysql.createConnection({
+  host: "teststock.cafe24app.com",
+  user: "dufqkd1004",
+  password: "capstone!!",
+  database: "dufqkd1004",
+  port: "3306",
+  multipleStatements: true,
+  typeCast: function (field, next) {
+    if (field.type == "VAR_STRING") {
+      return field.string();
+    }
+    return next();
+  },
+  multipleStatements: true,
+  // dateStrings: "date",
+  //socketPath: socket_path,
+});
 
 /*
 cron.schedule("* * * * * *", () => {
@@ -58,16 +58,16 @@ cron.schedule("* * * * * *", () => {
   });
 });
 */
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "111111",
-  database: "capstone",
-  port: "3306",
-  multipleStatements: true,
-  // dateStrings: "date",
-  //socketPath: socket_path,
-});
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "111111",
+//   database: "capstone",
+//   port: "3306",
+//   multipleStatements: true,
+//   // dateStrings: "date",
+//   //socketPath: socket_path,
+// });
 
 // db.connect(function (error) {
 //   if (error) {
