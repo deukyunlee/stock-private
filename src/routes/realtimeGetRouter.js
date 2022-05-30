84f42589b2894be3d19637de4a70d76846d8a1b4
deukyunlee <dukedev1004@gmail.com>
2022-05-31 00:05:25 +0900

    test

--- src/routes/realtimeGetRouter.js
+++ src/routes/realtimeGetRouter.js
@@ -3,16 +3,8 @@ const controller = require("../controllers/realtimeGetController");
 const validation = require("../funcs/validation");
 const { validate } = require("../funcs/validationMiddleware");
 
-router.get(
-  "/en/desc",
-  validate(validation.symbol),
-  controller.stock_realtime_desc_en_recent_top100_get
-);
-router.get(
-  "/kr/desc",
-  validate(validation.symbol),
-  controller.stock_realtime_desc_kr_recent_top100_get
-);
+router.get("/en/desc", controller.stock_realtime_desc_en_recent_top100_get);
+router.get("/kr/desc", controller.stock_realtime_desc_kr_recent_top100_get);
 // router.get("/en/asc", controller.stock_realtime_asc_en_recent_top100_get);
 // router.get("/kr/asc", controller.stock_realtime_kr_asc_recent_top100_get);
 
