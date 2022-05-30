0bf28025c0ba2852cec05bb5f73c35433cd8967b
deukyunlee <dukedev1004@gmail.com>
2022-05-30 23:39:47 +0900

    FEAT: Joi for search

--- src/funcs/validation.js
+++ src/funcs/validation.js
@@ -3,3 +3,8 @@ const Joi = require("joi");
 exports.symbol = Joi.object().keys({
   symbol: Joi.string().min(1).max(10).required(),
 });
+
+exports.search = Joi.object().keys({
+  method: Joi.string().min(2).max(6).required(),
+  keyword: Joi.string().min(1).max(10).required(),
+});
