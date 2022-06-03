384e864a87b0f4b67b8ace8073ed87d3bad5cd72
deukyunlee <dukedev1004@gmail.com>
2022-06-03 23:15:13 +0900

    test

--- web.js
+++ web.js
@@ -52,7 +52,7 @@ const swaggerSpec = swaggerJSDoc({
       {
         // url: "http://localhost:8001",
         // url: "http://teststock.cafe24app.com",
-        url: "https://www.stock-king.co.kr",
+        url: "http://www.stock-king.co.kr",
       },
     ],
   },
