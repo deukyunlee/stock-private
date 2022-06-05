ff7bfe5ead5c1473586d050933ccc98fb3db508c
deukyunlee <dukedev1004@gmail.com>
2022-06-06 01:18:28 +0900

    test
    Former-commit-id: 8a1f634dda631b441cc7b6ec7927140380251302

--- web.js
+++ web.js
@@ -409,7 +409,7 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-app.listen(process.env.PORT || 8001);
+app.listen(process.env.PORT || 8002);
 // const httpsServer = https.createServer(options, app);
 // httpsServer.listen(443, "stock-king.co.kr");
 // httpsServer.listen(443, "stock-king.co.kr");
